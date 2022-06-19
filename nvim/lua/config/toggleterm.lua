d170f90692078ac7ceccc2451c068f81563ffc8c
Jan Nemeček <jan@jannemecek.com>
2022-06-20 00:20:22 +0200

    Pretty toggleterm

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -7,6 +7,11 @@ require("toggleterm").setup{
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
+  direction = 'float',
   close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell -- change the default shell
+  shell = vim.o.shell, -- change the default shell
+  float_opts = {
+    border = 'curved',
+    winblend = 3,
+  }
 }
