5b3286861c0ce7f2c4edf2022bbabf349cbebc0f
Jan Nemeček <jan@jannemecek.com>
2022-08-17 15:20:31 +0200

    Add actual setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s ~/Developer/dotfiles/nvim/  ~/.config
+ln -s ~/Developer/dotfiles/.tmux.conf  ~
+ln -s ~/Developer/dotfiles/kitty.conf  ~/.config/kitty
+ln -s ~/Developer/dotfiles/.yabairc  ~
+ln -s ~/Developer/dotfiles/.skhdrc  ~
