cbdda572b2ee58ce525fd6ea62219f47f854eeae
Jan Nemeček <jan@jannemecek.com>
2024-06-04 16:53:04 +0200

    Lazy update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,7 @@
 return {
   { "tjdevries/colorbuddy.nvim" },
-  { "miloshadzic/hemisu.nvim" },
   { "folke/tokyonight.nvim" },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
