934b0a62829ce355eb32fad259eea7a5c5dee548
Jan Nemeček <jan@jannemecek.com>
2024-12-27 11:36:15 +0100

    Add Ghostty

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-ln -s ~/Developer/dotfiles/nvim/  ~/.config
-ln -s ~/Developer/dotfiles/.tmux.conf  ~
-ln -s ~/Developer/dotfiles/kitty.conf  ~/.config/kitty
-ln -s ~/Developer/dotfiles/.yabairc  ~
-ln -s ~/Developer/dotfiles/.skhdrc  ~
+ln -s ~/Developer/dotfiles/nvim/ ~/.config
+ln -s ~/Developer/dotfiles/.tmux.conf ~
+ln -s ~/Developer/dotfiles/kitty.conf ~/.config/kitty
+mkdir ~/.config/ghostty/
+ln -s ~/Developer/dotfiles/ghostty ~/.config/ghostty/config
+ln -s ~/Developer/dotfiles/.yabairc ~
+ln -s ~/Developer/dotfiles/.skhdrc ~
