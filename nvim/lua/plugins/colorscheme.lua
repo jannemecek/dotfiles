a8150fb182bffb552212fea979bd0dcb68ebc484
Jan Nemeček <jan@jannemecek.com>
2025-02-18 23:20:24 +0100

    Add Wakatime, updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,15 @@
 return {
   { "tjdevries/colorbuddy.nvim" },
-  { "folke/tokyonight.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 
   {
     "LazyVim/LazyVim",
