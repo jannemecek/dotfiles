e23689a3222d90a1b41c18efb1a36eb9c27e28bc
Jan Nemeček <jan@jannemecek.com>
2025-07-27 19:15:32 +0200

    Remove minuet debug

--- nvim/lua/plugins/minuet.lua
+++ nvim/lua/plugins/minuet.lua
@@ -1,10 +1,9 @@
 return {
   {
-    'milanglacier/minuet-ai.nvim',
+    "milanglacier/minuet-ai.nvim",
     config = function()
-      require('minuet').setup {
-        notify = 'debug',
-        provider = 'codestral',
+      require("minuet").setup({
+        provider = "codestral",
         request_timeout = 2.5,
         -- throttle = 1500, -- Increase to reduce costs and avoid rate limits
         -- debounce = 600, -- Increase to reduce costs and avoid rate limits
@@ -12,35 +11,35 @@ return {
           codestral = {
             optional = {
               max_tokens = 256,
-              stop = { '\n\n' },
+              stop = { "\n\n" },
             },
           },
-        }
-      }
-    end
+        },
+      })
+    end,
   },
   {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     optional = true,
     opts = {
       keymap = {
-        ['<A-y>'] = {
+        ["<A-y>"] = {
           function(cmp)
-            cmp.show { providers = { 'minuet' } }
+            cmp.show({ providers = { "minuet" } })
           end,
         },
       },
       sources = {
         -- if you want to use auto-complete
-        default =  { 'minuet' },
+        default = { "minuet" },
         providers = {
           minuet = {
-            name = 'minuet',
-            module = 'minuet.blink',
+            name = "minuet",
+            module = "minuet.blink",
             score_offset = 100,
           },
         },
       },
     },
-  }
+  },
 }
