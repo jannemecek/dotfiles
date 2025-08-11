45a6297cfb4e009c0252d70f473e7d93dae04b13
Jan Nemeček <jan@jannemecek.com>
2025-08-11 17:39:06 +0200

    TT

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -6,13 +6,13 @@ return {
       require("toggleterm").setup({
         open_mapping = [[<c-\>]],
         hide_numbers = true, -- hide the number column in toggleterm buffers
-        shade_filetypes = {},
+        auto_scroll = false,
         shade_terminals = true,
         start_in_insert = true,
         insert_mappings = true, -- whether or not the open mapping applies in insert mode
         terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
         persist_size = true,
-        direction = "float",
+        direction = "horizontal", -- was 'float'
         close_on_exit = true, -- close the terminal window when the process exits
         shell = vim.o.shell, -- change the default shell
         float_opts = {
